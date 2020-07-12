a05f867093dcae2655680d97d891b41adc199293
Travis Fox <travis.fox90@gmail.com>
2020-07-12 03:03:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
   
   def initialize(name)
     @name = name
-    save
+    save(name)
   end
   
   def self.all
@@ -26,7 +26,7 @@ class Dog
   
   private
   
-  def save
+  def save(name)
     @@all << name
   end
   
